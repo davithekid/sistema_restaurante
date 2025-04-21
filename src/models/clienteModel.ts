1843cabbfbc5713191cb5f8d543c25097fbf4054
davithekid <byjdrk.27@gmail.com>
2025-04-20 23:05:20 -0300

    feat: crud

--- src/models/clienteModel.ts
+++ src/models/clienteModel.ts
@@ -1,21 +1,121 @@
 import { getConnection } from "../database/connection";
+import { ResultSetHeader } from 'mysql2/promise';
 
-export async function readAll(table: string, where = null) {
-    const connection = await getConnection();
-    try {
-        let sql = `SELECT *FROM ${table}`;
-        if (where) {
-            sql += ` WHERE ${where}`
+export interface Cliente {
+    id_cliente?: string;
+    nome_cliente: string;
+}
+
+// criando um "padrão" de CRUD 
+export class ReadAllModel {
+    static async getAll(table: string, where?: string) {
+        const connection = await getConnection();
+        try {
+            let sql = `SELECT * FROM ${table}`;
+            if (where) {
+                sql += ` WHERE ${where}`;
+            }
+
+            const [rows] = await connection.execute(sql);
+            return rows;
+        } catch (err) {
+            console.error(`Erro ao buscar registros em ${table}: `, err);
+            throw err;
+        } finally {
+            connection.release();
         }
-        const [rows] = await connection.execute(sql);
-        return rows;
-    } catch (err) {
-        console.error('Erro ao ler registros: ', err);
-        throw err;
-    } finally {
-        connection.release();
     }
 }
+// função para ler um registro específico
+export class ReadModel {
+    static async read(table: string, where?: string) {
+        const connection = await getConnection();
+        try {
+            let sql = `SELECT * FROM ${table}`;
+            if (where) {
+                sql += ` WHERE ${where}`;
+            }
+
+            const [rows] = await connection.execute(sql);
+            if (Array.isArray(rows)) {
+                return rows[0] || null;
+            }
+
+            return null;
+
+        } catch (err) {
+            console.error('Erro ao ler clientes: ', err)
+            throw err;
+        } finally {
+            connection.release();
+        }
+    }
+}
+
+// função para inserir dados
+export class CreateModel {
+
+    async create(table: string, data: Record<string, any>) {
+        const connection = await getConnection();
+        try {
+            const columns = Object.keys(data).join(', ');
+            const placeholders = Array(Object.keys(data).length).fill('?').join(', ');
+            const sql = `INSERT INTO ${table} (${columns}) VALUES (${placeholders})`;
+
+            const values = Object.values(data);
+
+            const [result] = await connection.execute<ResultSetHeader>(sql, values);
+
+            return result.insertId;
+        } catch (err) {
+            console.error('Erro ao inserir registros: ', err);
+            throw err;
+        } finally {
+            connection.release();
+        }
+    }
+}
+
+export class UpdateModel {
+    static async update(table: string, data: Record<string, any>, where?: string) {
+        const connection = await getConnection();
+        try {
+            const set = Object.keys(data).map(column => `${column} = ?`).join(', ');
+
+            const sql = `UPDATE ${table} SET ${set} WHERE ${where}`;
+            const values = Object.values(data);
+
+            const [result] = await connection.execute<ResultSetHeader>(sql, [...values]);
+            return result.affectedRows
+
+        } catch (err) {
+            console.error('Erro ao atualizar registros: ', err)
+            throw err;
+        } finally {
+            connection.release();
+        }
+    }
+}
+
+// função para deletar um registro
+export class DeleteModel {
+   static  async deleteRecord(table:string, where?:string) {
+        const connection = await getConnection();
+        try {
+            const sql = `DELETE FROM ${table} WHERE ${where}`;
+            const [result] = await connection.execute<ResultSetHeader>(sql);
+            return result.affectedRows;
+        } catch (err) {
+            console.error('Erro ao deletar registros: ', err)
+            throw err;
+        } finally {
+            connection.release();
+        }
+    }
+}
+
+
+
 
 
 
--- src/tests/testRead.ts
+++ src/tests/testRead.ts
@@ -1,8 +1,8 @@
-import { readAll } from "../models/clienteModel"; // ajuste o caminho conforme necessário
+// import { readAll } from "../models/clienteModel"; // ajuste o caminho conforme necessário
 
-async function testar() {
-  const clientes = await readAll("cliente"); // ou readAll("cliente", "id_cliente = 1")
-  console.log("Clientes encontrados:", clientes);
-}
+// async function testar() {
+//   const clientes = await readAll("cliente"); // ou readAll("cliente", "id_cliente = 1")
+//   console.log("Clientes encontrados:", clientes);
+// }
 
-testar();
+// testar();
